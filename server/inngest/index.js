9da20ded00bfca83f442503b24936caad259efe3
chamikara01 <chmr6699@gmail.com>
2025-08-20 11:53:56 +0530

    typo errors

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -10,13 +10,13 @@ const syncUserCreation = inngest.createFunction(
     {event: 'clerk/user.created'},
     async({event})=>{
         const {id, first_name, last_name, email_addresses, image_url} = event.data
-        let username = email_addresses[0].email_address.spilt('@')[0]
+        let username = email_addresses[0].email_address.split('@')[0]
 
         //Check availability of username
         const user = await User.findOne({username})
 
         if(user){
-            username = username + Math.floor(Math.raandom() * 10000)
+            username = username + Math.floor(Math.random() * 10000)
         }
 
         const userData = {
