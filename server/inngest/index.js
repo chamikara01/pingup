0c49814aae7061c750b86f5cd5dd7604d57f88b9
chamikara01 <chmr6699@gmail.com>
2025-08-20 13:09:59 +0530

    minor changes

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -7,7 +7,7 @@ export const inngest = new Inngest({ id: "pingup-app" });
 // Inngest funtion to save user data to a database
 const syncUserCreation = inngest.createFunction(
     {id: 'sync-user-from-clerk'},
-    {event: 'clerk/user.created'},
+    {event: 'webhook-integration/user.created'},
     async({event})=>{
         const {id, first_name, last_name, email_addresses, image_url} = event.data
         let username = email_addresses[0].email_address.split('@')[0]
@@ -33,7 +33,7 @@ const syncUserCreation = inngest.createFunction(
 // Inngest function to update user data in database
 const syncUserUpdation = inngest.createFunction(
     {id: 'update-user-from-clerk'},
-    {event: 'clerk/user.updated'},
+    {event: 'webhook-integration/user.updated'},
     async({event})=>{
         const {id, first_name, last_name, email_addresses, image_url} = event.data
 
@@ -49,7 +49,7 @@ const syncUserUpdation = inngest.createFunction(
 // Inngest function to delete user data in database
 const syncUserDeletion = inngest.createFunction(
     {id: 'delete-user-with-clerk'},
-    {event: 'clerk/user.deleted'},
+    {event: 'webhook-integration/user.deleted'},
     async({event})=>{
         const {id} = event.data
         await User.findByIdAndDelete(id)
